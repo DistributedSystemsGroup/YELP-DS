2f6294aa636a7cece6e0d9856431b87c0a370d42
KeheCAI <caikehe@gmail.com>
2014-09-25 20:30:51 +0200

    revise SVM by using linear, rbf and polynomial kernels

--- Scikit_SVM_Classification.py
+++ Scikit_SVM_Classification.py
@@ -17,10 +17,17 @@ def Scikit_SVM_Classification(filename):
             features.append(item["histogram"])
             labels.append(item["rating"])
 
-    # train the model
+    # train the model using linear kernel, RBF kernel and polynomial
+    # kernel respecively
     training_Features = features[0:800]
     training_Labels  = labels[0:800]
-    Scikit_SVM_Model = svm.SVC()
+    kernel_Index = 3 #(kernel can be 1, 2, 3)
+    if kernel_Index == 1:
+        Scikit_SVM_Model = svm.SVC(kernel='linear')
+    elif kernel_Index == 2:
+        Scikit_SVM_Model = svm.SVC(kernel='rbf')
+    elif kernel_Index == 3:
+        Scikit_SVM_Model = svm.SVC(kernel='poly', degree=3)
     Scikit_SVM_Model.fit(training_Features, training_Labels)
 
     # testing
@@ -28,7 +35,8 @@ def Scikit_SVM_Classification(filename):
     testing_Labels = labels[800:1000]
 
     predict_Labels = Scikit_SVM_Model.predict(testing_Features)
-    print predict_Labels
+    accurancy = Scikit_SVM_Model.score(testing_Features, testing_Labels)
+    print accurancy
 
 def main():
     starttime = strftime("%Y-%m-%d %H:%M:%S",gmtime())
