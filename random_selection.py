a1920136caa1e13eb5b79419d7da62faa1bdbfaf
KeheCAI <caikehe@gmail.com>
2014-09-25 19:40:27 +0200

    add random selection function

---
+++ random_selection.py
@@ -0,0 +1,19 @@
+
+import io, os
+import random
+
+numberOfData = 1125458
+numberOfSample = 1000
+
+inputfile = 'yelp_academic_dataset_review.json'
+outputfile = 'samples.json'
+
+randomSelectionList = random.sample(xrange(0, numberOfData), numberOfSample)
+
+if os.path.isfile(outputfile):
+    os.remove(outputfile)
+with io.open(outputfile, 'a', encoding='utf-8') as outfile:
+    with open(inputfile) as fileobject:
+       for i, line in enumerate(fileobject):
+           if i in randomSelectionList:
+               outfile.write(unicode(line))
