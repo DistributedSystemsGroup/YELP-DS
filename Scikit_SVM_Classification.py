0c949d6e06d76b21bcedb69b56edb523f29d1772
KeheCAI <caikehe@gmail.com>
2014-09-28 23:19:10 +0200

    Add DecionTree and RandomForest methods

--- Scikit_SVM_Classification.py
+++ Scikit_SVM_Classification.py
@@ -3,6 +3,8 @@
 
 from time import gmtime, strftime
 from sklearn import svm
+from sklearn import tree
+from sklearn import ensemble
 import json
 
 features = []
@@ -43,7 +45,7 @@ def Scikit_SVM_Regression(filename):
         data = json.load(data_file)
         for item in data:
             features.append(item['histogram'])
-            features.append(item['rating'])
+            labels.append(item['rating'])
 
     # fit regression model
     training_Features = features[0:800]
@@ -65,11 +67,56 @@ def Scikit_SVM_Regression(filename):
     print predict_Labels
     print testing_Labels-predict_Labels
 
+def Scikit_DecisionTree_Classification(filename):
+    # read training data and testing data
+    with open(filename) as data_file:
+        data = json.load(data_file)
+        for item in data:
+            features.append(item['histogram'])
+            labels.append(item['rating'])
+
+    # fit classification model
+    training_Features = features[0:800]
+    training_Labels  = labels[0:800]
+    Scikit_DecisionTree_Model = tree.DecisionTreeClassifier()
+    Scikit_DecisionTree_Model.fit(training_Features, training_Labels)
+
+    # testing
+    testing_Features = features[800:1000]
+    testing_Labels = labels[800:1000]
+
+    predict_Labels = Scikit_DecisionTree_Model.predict(testing_Features)
+    print predict_Labels
+    print testing_Labels-predict_Labels
+
+def Scikit_RandomForest_Classification(filename):
+    # read training data and testing data
+    with open(filename) as data_file:
+        data = json.load(data_file)
+        for item in data:
+            features.append(item['histogram'])
+            labels.append(item['rating'])
+
+    # fit classification model
+    training_Features = features[0:800]
+    training_Labels  = labels[0:800]
+    Scikit_RandomForest_Model = ensemble.RandomForestClassifier(n_estimators=10)
+    Scikit_RandomForest_Model.fit(training_Features, training_Labels)
+
+    # testing
+    testing_Features = features[800:1000]
+    testing_Labels = labels[800:1000]
+
+    predict_Labels = Scikit_RandomForest_Model.predict(testing_Features)
+    print predict_Labels
+    print testing_Labels-predict_Labels
 
 def main():
     starttime = strftime("%Y-%m-%d %H:%M:%S",gmtime())
     Scikit_SVM_Classification('output_split1000.json')
     Scikit_SVM_Regression('output_split1000.json')
+    Scikit_DecisionTree_Classification('output_split1000.json')
+    Scikit_RandomForest_Classification('output_split1000.json')
     endtime = strftime("%Y-%m-%d %H:%M:%S",gmtime())
     print(starttime)
     print(endtime)
