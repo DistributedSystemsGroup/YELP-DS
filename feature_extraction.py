be4dff63b093ca4046eadee9314deaea3e0bdf84
mvtuong <mvtuong90@gmail.com>
2014-12-22 19:47:13 +0100

    modify feature_extraction.py

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,8 +1,11 @@
-import re
-import io, json
+from sklearn.feature_extraction.text import CountVectorizer
+from numpy.linalg import norm
 
-import smtplib
-from email.mime.text import MIMEText
+from sklearn.preprocessing import normalize, scale
+from sklearn import preprocessing
+import numpy as np
+import re, io, json
+from string import punctuation
 
 from time import gmtime, strftime
 from datetime import datetime
@@ -17,7 +20,7 @@ def words(text):
         normed = re.sub('[^a-z]', '', word.lower())
         if normed:
             yield normed
-            
+
 def buildingHistogram6Bins(keyword1, keyword2, result):
     if keyword1 == 'strongsubj' and keyword2 == 'positive':
         result[0]+=1
@@ -65,86 +68,128 @@ def buildingHistogramVote(votes, result):
 def buildingHistogramLength(length, result):
     result [9] = length
 
-def sendMail(mailmessage):
-    # Define email addresses to use
-    addr_to   = 'caikehe@gmail.com'
-    addr_from = 'walleve@mail.com'
- 
-    # Define SMTP email server details
-    smtp_server = 'smtp.mail.com'
-    smtp_user   = 'walleve@mail.com'
-    smtp_pass   = 'test123456'
+starttime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
 
-    # Construct email
-    msg = MIMEText(mailmessage)
-    msg['To'] = addr_to
-    msg['From'] = addr_from
-    msg['Subject'] = 'Notification Email'
+filenames = ['data/input/1StarsSamples.json', 'data/input/2StarsSamples.json', 'data/input/3StarsSamples.json', 'data/input/4StarsSamples.json', 'data/input/5StarsSamples.json']
 
-    # Send the message via an SMTP server
-    s = smtplib.SMTP(smtp_server)
-    s.login(smtp_user,smtp_pass)
-    s.sendmail(addr_from, addr_to, msg.as_string())
-    s.quit()        
+vectorizer = CountVectorizer(input='filename', ngram_range=(1,3), stop_words='english', strip_accents='unicode', token_pattern=ur'\b\w+\b')
 
-class Histogram():
-    def run(self, filename):
+dtm = vectorizer.fit_transform(filenames).toarray()
+dtm = scale(dtm)
+vocab = np.array(vectorizer.get_feature_names())
+
+_vectorizer = CountVectorizer(input='content', ngram_range=(1,3), stop_words='english', strip_accents='unicode', token_pattern=ur'\b\w+\b')
+analyze = _vectorizer.build_analyzer()
+
+with open('../data/input/10000samples.json') as fileobject, open("../data/dictionary/mydictionary_2bins.json") as dicObject2Bins, open("../data/dictionary/mydictionary_3bins.json") as dicObject3Bins, open("../data/dictionary/mydictionary_6bins.json") as dicObject6Bins:
+    dicData2Bins = json.load(dicObject2Bins)
+    dicData3Bins = json.load(dicObject3Bins)
+    dicData6Bins = json.load(dicObject6Bins)
+    listOfHistogramAndRating = []
+
+    i1Star = 0
+    i2Star = 0
+    i3Star = 0
+    i4Star = 0
+    i5Star = 0
+
+    listOfHistogram1Star = []
+    listOfHistogram2Star = []
+    listOfHistogram3Star = []
+    listOfHistogram4Star = []
+    listOfHistogram5Star = []
+
+
+    for i, line in enumerate(fileobject):
+
+        print(i)
+
+        if line == '\n':
+            break
+        data = json.loads(line)
+        #Descripton [0-spos, 1-wpos, 2-sneu, 3-wneu, 4-sneg, 5-wneg, 6-pos, 7-neg, 8-negation, 9-len, 10-day, 11-vote, 12-ps, 13-ns]
+        result =    [0     , 0     , 0     , 0     , 0     , 0     , 0    , 0    , 0         , 0    , 0     , 0      , 0.0  , 0.0  ]
+        length = 0
+        text = data["text"]
+        for word in words(text):
+            length += 1
+
+            for item in dicData2Bins:
+                if word == item["word"]:
+                    buildingHistogram2Bins(item["pos"], item["neg"], result)
+            for item in dicData3Bins:
+                if word == item["word"]:
+                    buildingHistogram3Bins(item["priorpolarity"], result)
+            for item in dicData6Bins:
+                if word == item["word"]:
+                    buildingHistogram6Bins(item["type"], item["priorpolarity"], result)
         
-        starttime = strftime("%Y-%m-%d %H:%M:%S", gmtime())
+        buildingHistogramDay(data["date"], result)
+
+        buildingHistogramVote(data["votes"], result)
+                
+        buildingHistogramLength(length, result)
+
+        character_count = len(text)
+        uppercase_count = sum(1 for c in text if c.isupper())
+        lowercase_count = sum(1 for c in text if c.islower())
+        punctuation_count = sum(1 for c in text if c in punctuation)
+        alphabetic_count = sum(1 for c in text if c.isalpha())
+        numeric_count = sum(1 for c in text if c.isnumeric())
         
-        with open("data/dictionary/mydictionary_3bins.json") as dicObject3Bins, open("data/dictionary/mydictionary_6bins.json") as dicObject6Bins, open("data/dictionary/mydictionary_2bins.json") as dicObject2Bins, open(filename) as fileObject:    
-            #dicData3Bins = json.load(dicObject3Bins)
-            #dicData6Bins = json.load(dicObject6Bins)
-            dicData2Bins = json.load(dicObject2Bins)
-            listOfHistogramAndRating = []
-            
-            for i, line in enumerate(fileObject):
-                if line == '\n':
-                    break
-                #if i not in xrange(0, 100000):
-                    #break
-                print(i)
+       
+        #Fearures lesection        
+        final_result = []
+               
 
-                data = json.loads(line)
-                #Descripton [0-spos, 1-wpos, 2-sneu, 3-wneu, 4-sneg, 5-wneg, 6-pos, 7-neg, 8-negation, 9-len, 10-day, 11-vote, 12-ps, 13-ns]
-                result =    [0     , 0     , 0     , 0     , 0     , 0     , 0    , 0    , 0         , 0    , 0     , 0      , 0.0  , 0.0  ]
-                length =  0
-                for word in words(data["text"]):
-                    """length += 1
-                    for item in dicData3Bins:
-                        if word == item["word"]:
-                            buildingHistogram3Bins(item["priorpolarity"], result)
-                    for item in dicData6Bins:
-                        if word == item["word"]:
-                            buildingHistogram6Bins(item["priorpolarity"], result)"""
-                    for item in dicData2Bins:
-                        if word == item["word"]:
-                            buildingHistogram2Bins(item["pos"], item["neg"], result)
+        final_result.append(result[0])
+        final_result.append(result[1])
+        final_result.append(result[2])
+        final_result.append(result[3])
+        final_result.append(result[4])
+        final_result.append(result[5])
+        final_result.append(result[6])
+        final_result.append(result[7])
+        final_result.append(result[8])
+        final_result.append(result[9])
+        final_result.append(result[10])
+        final_result.append(result[11])
+        final_result.append(result[12])
+        final_result.append(result[13])
 
-                #buildingHistogramDay(data["date"], result)
+        final_result.append(character_count) #14
+        final_result.append(uppercase_count) #15
+        final_result.append(lowercase_count) #16
+        final_result.append(punctuation_count) #17
+        final_result.append(alphabetic_count) #18
+        final_result.append(numeric_count) #19
 
-                #buildingHistogramVote(data["votes"], result)
+        analyzed = analyze(text)
+        #Descripton [20-1star, 21-2star, 22-3star, 23-4star, 24-5star]
+        resultBOW = [0       , 0       , 0       , 0       , 0       ]
+        
+        for item in analyzed:
+            feature_index = vectorizer.vocabulary_.get(item)
+            if feature_index:
+                item_histogram = dtm[:, feature_index]
                 
-                #buildingHistogramLength(length, result)
-                
-                #Fearures lesection
-                final_result = []
-                final_result.append(result[12])
-                final_result.append(result[13])
-                listOfHistogramAndRating.append({'rating': data["stars"], 'histogram': final_result})
-                
+                resultBOW = [x + y for x, y in zip(resultBOW, item_histogram)]
+        
+        for item in resultBOW:
+            if sum(resultBOW) != 0:
+                item = item*1.0/sum(resultBOW)
+            final_result.append(item)
 
-            with io.open('data/output/histogram.json', 'w', encoding='utf-8') as outfile:
-                outfile.write(unicode(json.dumps(listOfHistogramAndRating, ensure_ascii=False)))
-
-        print "FINISHED"
-
-        endtime = strftime("%Y-%m-%d %H:%M:%S", gmtime())       
-
-        mailmessage = 'This is a notification email to show that the task is completed\n' + "Start time: " + starttime + " End time: " + endtime + "\n"
-        print(mailmessage)
-        #sendMail(mailmessage)    
+        
+        listOfHistogramAndRating.append({'rating': data["stars"], 'histogram': final_result})
+    with io.open('data/output/histogram_allFeatures.json', 'w', encoding='utf-8') as outfile:
+        outfile.write(unicode(json.dumps(listOfHistogramAndRating, ensure_ascii=False)))
 
 
-if __name__ == "__main__":
-    Histogram().run("data/input/10000samples.json")
+print "FINISHED"
+
+endtime = strftime("%Y-%m-%d %H:%M:%S", gmtime())       
+
+mailmessage = 'This is a notification email to show that the task is completed\n' + "Start time: " + starttime + " End time: " + endtime + "\n"
+print(mailmessage)
+
